37c1e060777e317130031781de089f62133bf47c
Paulo Araújo <paulojosejr95@gmail.com>
2021-02-15 09:42:12 +0000

    update ex

---
+++ Javascript/jsex01/products/script.js
@@ -0,0 +1,13 @@
+
+window.addEventListener('load', function(){
+    let form = document.getElementsByTagName('form')[0]
+   let elem2 = input2[1].outerHTML
+
+    for (let i = 0; i < input.length; i++) {
+        let element = input[i]
+        alert(element)
+    }
+    
+})
+
+
